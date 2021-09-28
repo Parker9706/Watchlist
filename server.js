c2a2fc49438fa36c9f8fe1f65d02c4c82b19f704
Parker Hutcheson <pdhutcheson@gmail.com>
2021-09-28 10:52:27 -0700

    "Added Core React Components, Intiialized WebPack, added NodeMon, Express, and a basic Server.js File"

---
+++ server.js
@@ -0,0 +1,16 @@
+//server.js file
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  // res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000...')
+})
