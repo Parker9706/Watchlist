4d1a78f173bef0a49a9919320933a5b761ddca5e
Parker9706 <pdhutcheson@gmail.com>
2022-02-06 22:24:30 -0600

    Added title, heading, and adjusted some routing

--- app/App.jsx
+++ app/App.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Watchlist from './/Watchlist.jsx';
-import CreateDramaCard from './/CreateDramaCard.jsx';
+import MostPopular from './MostPopular.jsx';
 
 import './/styles.css';
 
@@ -10,22 +9,12 @@ const App = props => {
   return (
     <div className="router">
       <main>
-        {/*
-            NOTE: The syntax below is for React-Router
-              - A helpful library for routing with a React app.
-              You can learn more about this at:
-              https://reacttraining.com/react-router/web/guides/quick-start
-        */}
+        <h2 id="main-header">🇰🇷 KOREAN DRAMAS</h2>
         <Switch>
           <Route
             exact
             path="/"
-            component={Watchlist}
-          />
-          <Route
-            exact
-            path="/create"
-            component={CreateDramaCard}
+            component={MostPopular}
           />
         </Switch>
       </main>
